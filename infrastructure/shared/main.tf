aa1fa546ada3a1215c76f4468d057f74a011fb23
Edwin Moedano <edwin.moedano@caylent.com>
2026-05-12 10:17:50 -0600

    feat: readme added

--- infrastructure/shared/main.tf
+++ infrastructure/shared/main.tf
@@ -102,6 +102,7 @@ resource "aws_iam_policy" "github_actions" {
           "s3:PutObject",
           "s3:GetObject",
           "s3:DeleteObject",
+          "s3:GetBucketRequestPayment"
         ]
         Resource = "*"
       },
